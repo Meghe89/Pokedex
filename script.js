4850fa1341c6436dd9421e2339b6345f5dc70e32
Michele Cozzola <cozzolamichele89@gmail.com>
2022-04-05 17:19:44 +0200

    fix errore display

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const searchIcon = document.querySelector('#search-icon');
-
 const input = document.querySelector("#p-name");
-
-
 const noData = document.querySelector("#no-data");
 const dataContainer = document.querySelector("#data-container");
 
-//let lastLenght = input.value.length
+
 input.addEventListener("keydown", (e) => {
   if (e.code == "Enter") {
-    /* let filtered = ads.filter(ad =>ad.name.toLowerCase().includes(input.value.toLowerCase()))  */
     const pokemonName = input.value.toLowerCase();
-
-    input.value = "";
-
-    
+    input.value = "";  
 
     pokemonData(pokemonName)
   }
@@ -23,16 +16,12 @@ input.addEventListener("keydown", (e) => {
 searchIcon.addEventListener('click',()=>{
   const pokemonName = input.value.toLowerCase()
   input.value = "";
-
     
-
  pokemonData(pokemonName)
 })
 
 
 function pokemonData(pokemonName){
-
-
   fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
@@ -40,9 +29,7 @@ function pokemonData(pokemonName){
       if (data) {
         dataContainer.classList.remove('d-none')
         noData.classList.add('d-none')
-      }
-
-     
+      } 
 
       //img
       const pokeImg = (document.querySelector(
@@ -50,143 +37,128 @@ function pokemonData(pokemonName){
       ).src = `${data.sprites.front_default}`);
 
       //number
-
       const pokeNum = (document.querySelector(
         "#poke-num"
       ).innerHTML = `${data.id}`);
 
       //type
       const type = `${data.types[0].type.name}`
-
       const pokeType1 = (document.querySelector("#poke-type1"));
 
       pokeType1.innerHTML = type ;
-
         i = document.createElement('i')
 
         let solid = 'fa-solid'
       if (type == 'electric') {
-        i.classList.add(solid, 'fa-bolt-lightning', 'yellow');
-        
-        
-      }
-      if (type == 'grass') {
-        
-        i.classList.add(solid, 'fa-leaf', 'green');
-        
-        
-      }
-      if (type == 'fire') {
-        
-        i.classList.add(solid, 'fa-fire-flame-curved', 'red');
-        
-        
-      }
-      if (type == 'water') {
-        
-        i.classList.add(solid, 'fa-droplet', 'blue');
-        
-        
-      }
-      if (type == 'bug') {
-        
-        i.classList.add(solid, 'fa-bug', 'green');
-        
-        
-      }
-      if (type == 'poison') {
-        
-        i.classList.add(solid, 'fa-skull-crossbones', 'purple');
-        
-        
-      }
-      if (type == 'ground') {          
-        i.classList.add(solid, 'fa-circle', 'brown');
-        
-      }
-      if (type == 'fairy') {          
-        i.classList.add(solid, 'fa-wand-sparkles', 'light-blue');
-        
-      }
-      if (type == 'fighting') {          
-        i.classList.add(solid, 'fa-hand-fist');
-        
-      }
-      if (type == 'psychic') {          
-        i.classList.add(solid, 'fa-brain', 'psychic');
-        
-      }
-      if (type == 'rock') {          
-        i.classList.add(solid, 'fa-circle', 'rock');
-        
-      }
-      if (type == 'ghost') {          
-        i.classList.add(solid, 'fa-ghost', 'ghost');
-        
-      }
-      if (type == 'ice') {          
-        i.classList.add(solid, 'fa-snowflake', 'snowflake');
-        
-      }
-      if (type == 'dragon') {          
-        i.classList.add(solid, 'fa-dragon');
-        
+        i.classList.add(solid, 'fa-bolt-lightning', 'yellow');  
       }
 
+      if (type == 'grass') {        
+        i.classList.add(solid, 'fa-leaf', 'green');
+       }
+
+      if (type == 'fire') {
+       i.classList.add(solid, 'fa-fire-flame-curved', 'red');
+      }
+
+      if (type == 'water') {
+        i.classList.add(solid, 'fa-droplet', 'blue');
+      }
+
+      if (type == 'bug') {                
+        i.classList.add(solid, 'fa-bug', 'green');           
+      }
+
+      if (type == 'poison') {        
+        i.classList.add(solid, 'fa-skull-crossbones', 'purple');       
+      }
+
+      if (type == 'ground') {          
+        i.classList.add(solid, 'fa-circle', 'brown');
+      }
+
+      if (type == 'fairy') {          
+        i.classList.add(solid, 'fa-wand-sparkles', 'light-blue');
+      }
+
+      if (type == 'fighting') {          
+        i.classList.add(solid, 'fa-hand-fist');
+      }
+
+      if (type == 'psychic') {          
+        i.classList.add(solid, 'fa-brain', 'psychic');
+      }
+
+      if (type == 'rock') {          
+        i.classList.add(solid, 'fa-circle', 'rock');
+      }
+
+      if (type == 'ghost') {          
+        i.classList.add(solid, 'fa-ghost', 'ghost');
+      }
+
+      if (type == 'ice') {          
+        i.classList.add(solid, 'fa-snowflake', 'snowflake');
+      }
+
+      if (type == 'dragon') {          
+        i.classList.add(solid, 'fa-dragon');
+      }
+
+      if(type == 'flying'){
+        i.classList.add(solid, 'fa-feather-pointed')
+      }
+
+      if(type == 'steel'){
+        i.classList.add(solid, 'fa-shield', 'gray')
+      }
 
       pokeType1.appendChild(i)
 
       //name
-
       const pokeName = (document.querySelector(
         "#poke-name"
       ).innerHTML = `${data.name}`);
 
       //hp
-
       const pokeHp = (document.querySelector(
         "#poke-hp"
       ).innerHTML = `${data.stats[0].base_stat}`);
 
       //attack
-
       const pokeAttack = (document.querySelector(
         "#poke-attack"
       ).innerHTML = `${data.stats[1].base_stat}`);
 
       //defense
-
       const pokeDefense = (document.querySelector(
         "#poke-defense"
       ).innerHTML = `${data.stats[2].base_stat}`);
+      
       //special-attack
-
       const pokeSpecialAttack = (document.querySelector(
         "#poke-special-attack"
       ).innerHTML = `${data.stats[3].base_stat}`);
-      //special-defense
 
+      //special-defense
       const pokeSpecialDefense = (document.querySelector(
         "#poke-special-defense"
       ).innerHTML = `${data.stats[4].base_stat}`);
+      
       //speed
-
       const pokeSpeed = (document.querySelector(
         "#poke-speed"
       ).innerHTML = `${data.stats[5].base_stat}`);
 
       //weight
-
       const pokeWeight = (document.querySelector(
         "#poke-weight"
       ).innerHTML = `${data.weight}`);
     })
-    .catch(()=>{dataContainer.style.display = 'none'
-      noData.style.display = 'block'
-      noData.style.marginTop = '70px'
+    .catch(()=>{
+      dataContainer.classList.add('d-none')
+      noData.classList.remove('d-none')      
       let notFound = document.querySelector('#not-found')
-      notFound.innerHTML = 'Pokemon not Found!'
-      notFound.style.textAlign = 'center'
-         
+      notFound.innerHTML = 'Pokemon not Found!'         
     });
 }
